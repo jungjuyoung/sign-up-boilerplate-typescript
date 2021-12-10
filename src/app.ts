1f3d4295a7f76b005f6fc7d4336fe53f5ac83f2b
jungjuyoung <nwwn1004@naver.com>
2021-12-10 16:59:54 +0900

    feat: generated token with jsonwebtoken

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import routes from './routes';
 import mongoose from 'mongoose';
 import config from 'config';
 import jwt from 'jsonwebtoken';
-import UserModel, { IUser, IUserModel } from './models/User';
+import UserModel, { IUser, IUserCallback } from './models/User';
 
 const app = express();
 const port: number = 5000;
@@ -27,7 +27,7 @@ app.post('/register', (req: Request, res: Response, next: NextFunction) => {
   // 그것을 데이터 베이스에 넣어준다.
   const user = new UserModel(req.body);
   // User Schema 쪽에 pre로 미들웨어 작업 후 실행
-  console.log(`user save`);
+  console.log(`user.save로 유저가 등록`);
   user.save((err: any) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
@@ -36,34 +36,40 @@ app.post('/register', (req: Request, res: Response, next: NextFunction) => {
 
 app.post('/login', (req: Request, res: Response, next: NextFunction) => {
   // 입력한 email이 DB에 있는지 찾는다.
-  UserModel.findOne({ email: req.body.email }, (err: any, user: IUserModel) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: '제공된 이메일에 해당하는 유저가 없습니다.',
-      });
-    }
-
-    // 입력한 email이 DB에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch) {
+  UserModel.findOne(
+    { email: req.body.email },
+    (err: Error | null, user: IUserCallback) => {
+      if (!user) {
         return res.json({
           loginSuccess: false,
-          message: '비밀번호가 틀렸습니다.',
+          message: '제공된 이메일에 해당하는 유저가 없습니다.',
         });
       }
 
-      // 비밀번호까지 맞다면 토큰을 생성하기.
-      // user.generateToken((err, user) => {
-      //   if (err) return res.status(400).send(err);
-      //   // token을 쿠키에 저장.
-      //   res
-      //     .cookie('x_auth', user.token)
-      //     .status(200)
-      //     .json({ loginSuccess: true, userId: user._id });
-      // });
-    });
-  });
+      // 입력한 email이 DB에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        console.log(`app comparePassword`);
+
+        if (!isMatch) {
+          return res.json({
+            loginSuccess: false,
+            message: '비밀번호가 틀렸습니다.',
+          });
+        }
+
+        // 비밀번호까지 맞다면 토큰을 생성하기.
+        user.generateToken((err, user) => {
+          console.log(`app user: ${user}`);
+          if (err) return res.status(400).send(err);
+          // token을 쿠키에 저장.
+          res
+            .cookie('x_auth', user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id });
+        });
+      });
+    },
+  );
 });
 
 app.listen(port, async () => {
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,6 @@
 import mongoose, { Schema, Document, model } from 'mongoose';
 import bcrypt from 'bcrypt';
 import config from 'config';
-import { NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export interface IUser {
@@ -12,14 +11,14 @@ export interface IUser {
   role: number;
   token: string;
   tokenExp: number;
+  _id: string;
 }
-
-export interface IUserModel extends Document, IUser {
-  comparePassword(
-    password: string,
-    callback: (err: Error | null, isMatch: boolean | null) => void,
-  ): void;
-  generateToken(err: Error | null, user: IUser): void;
+export interface IUserCallback extends IUser {
+  comparePassword: (
+    plainPassword: string,
+    cb: (err: Error | null, isMatch: boolean | null) => void,
+  ) => void;
+  generateToken: (cd: (err: Error, user: IUser) => void) => void;
 }
 
 const userSchema = new mongoose.Schema({
@@ -52,6 +51,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
+  // console.log('User모델 pre save');
   const user = this;
 
   if (!user.isModified('password')) {
@@ -64,31 +64,40 @@ userSchema.pre('save', async function (next) {
   // salt 생성.
   const hash = await bcrypt.hashSync(user.password, salt); // hash화.
 
-  console.log(`salt: ${salt}, hash: ${hash}`);
+  // console.log(`salt: ${salt}, hash: ${hash}`);
   user.password = hash;
   return next();
 });
 
+// plainPassword와 db에 암호화된 비밀번호를 비교
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-  // plainPasswrd: 1234567, db에 암호화된 비밀번호를 비교.
-  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+  console.log(`Schema.methods.comparePassword`);
+  // plainPassword: 1234567 vs 암호화된 비밀번호 비교
+  bcrypt.compare(
+    plainPassword,
+    this.password,
+    (err: any, isMatch: boolean | null) => {
+      if (err) return cb(err, null);
+      cb(null, isMatch);
+    },
+  );
 };
 
 userSchema.methods.generateToken = function (cb) {
+  console.log(`Schema.methods.generateTOke`);
   // jsonwebtoken을 이용해서 token 생성하기
   let user = this;
-  console.log(`methods generateToken user: ${user}`);
+  const sign = config.get<string>('sign');
+  console.log(`methods generateToken user: ${user}, sign: ${sign}`);
 
-  const token = jwt.sign(user._id, 'secretToken');
+  const token = jwt.sign(user._id.toHexString(), sign);
   user.token = token;
+
   user.save((err: Error | null, user: IUser) => {
     if (err) return cb(err);
     cb(null, user);
   });
 };
 
-const UserModel = mongoose.model<IUserModel>('User', userSchema);
+const UserModel = mongoose.model<IUserCallback>('User', userSchema);
 export default UserModel;
